31f243b66a397b3559a1883f63d841dccdb2224d
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-04-05 12:44:55 +0700

    render movie details with api call

--- src/movies/movieDetail.js
+++ src/movies/movieDetail.js
@@ -2,7 +2,29 @@ const API_ENDPOINT = 'http://localhost:3000';
 const urlComponents = location.pathname.split('/');
 const id = urlComponents[urlComponents.length - 2];
 
-getMovieById(id);
+const addWatchlistButton = document.querySelector('#add-to-watchlist');
+const posterImage = document.querySelector('#poster-image');
+const genresContainer = document.querySelector('#genres');
+const movieDescription = document.querySelector('#movie-description');
+const movieRating = document.querySelector('#movie-rating');
+const movieTrailer = document.querySelector('#movie-trailer');
+
+renderMovie();
+
+async function renderMovie() {
+  const res = await getMovieById(id);
+
+  if (!res.success) {
+    console.log(res.message);
+    return;
+  }
+
+  posterImage.src = res.data.image;
+  genresContainer.innerHTML = res.data.genre.map((genre) => genreComponent(genre)).join('');
+  movieDescription.innerText = res.data.synopsis;
+  movieRating.innerText = res.data.rating;
+  movieTrailer.src = res.data.trailer;
+}
 
 async function getMovieById(id) {
   try {
@@ -17,4 +39,10 @@ async function getMovieById(id) {
   } catch (error) {
     console.log(error);
   }
+}
+
+function genreComponent(genre) {
+  return `
+    <span class="block px-3 py-1 border-2 border-slate-700 rounded-full">${genre}</span>
+  `;
 }
